b6110d75b612223e84e1e47e11180463dd884825
Joseph Birara <josephbirara92@gmail.com>
2022-04-08 05:34:32 -0700

    betty style addede

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the alphabet in lowercase,
- *   * followed by a new line
- *    * Return: Always 0 (Success)
- *     */
+ * main - prints the alphabet in lowercase,
+ *
+ * lowed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		char ch;
