782cc984b44290d4711ce01d125201b84be6b021
Joseph Birara <josephbirara92@gmail.com>
2022-04-08 05:44:30 -0700

    betty style addede

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-		char ch;
-		
-		for (ch = 'a'; ch <= 'z'; ch++)
-			{
-				putchar(ch);
-			}
-		putchar('\n');
-		return (0);
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+	putchar('\n');
+	return (0);
 }
