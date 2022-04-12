b7acef8d690706a488295dc5d47103019cf82d51
Joseph Birara <josephbirara92@gmail.com>
2022-04-12 10:49:47 -0700

    task to c code added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints  _puchar
+ */
+void print_alphabet_x10(void)
+{
+	char le;
+
+	int k;
+
+	for (k = 1; k <= 10; k++)
+	{
+		for (le = 'a'; le <= 'z'; le++)
+		{
+			_putchar(le);
+		}
+		_putchar('\n';
+	}
+}
