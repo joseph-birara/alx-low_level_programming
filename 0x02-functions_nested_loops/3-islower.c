32828e57bcbc9e7a60c4fa0246ed5c620c77c554
Joseph Birara <josephbirara92@gmail.com>
2022-04-12 11:03:18 -0700

    c code added to file 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * int _islower- checks for lowercase 
+ * _islower- checks for lowercase
  * @c: letter to bechecked
  * Return: returns 0
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
 
