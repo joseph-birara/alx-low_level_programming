4db7afdc72833d3d209b8febe3de9f097ff7563f
Joseph Birara <josephbirara92@gmail.com>
2022-04-12 10:42:49 -0700

    task 1 solved

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet- prints leworcase alphabet
+ */
+void print_alphabet(void)
+{
+	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+	{
+		_putchar(le);
+	}
+	_putchar('\n');
+}
