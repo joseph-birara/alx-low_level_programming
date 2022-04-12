cf5cb98b344692c7c065a52ad462a24016675d6c
Joseph Birara <josephbirara92@gmail.com>
2022-04-12 11:24:15 -0700

    abs function defined

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs-computes the absolute value
+ * @n: number to be computed
+ * Return: nabs of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1*n);
+	else
+		return (n);
+}
