2221a7698072e0d83061c0a9fb946b75e9b3a68e
Joseph Birara <josephbirara92@gmail.com>
2022-04-18 13:15:14 -0700

    bug fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,17 @@
-
-  
-#include "holberton.h"
-
+#include "main.h"
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
+ * puts2 - prints every character with a new line
+ * @str: a character pointer
+ * Return: nothing
  */
-
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i]; i++)
 	{
-		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
+		if ((i % 2) == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
