923fbf2e7e18c2e714f0162e488c3809efd5fea9
joseph-birara <josephbirara92@gmail.com>
2022-07-30 00:34:19 +0300

    bug fixed

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,21 +1,9 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-
-/*
- * file: hash_tables.h
- *
- * Desc: This header file contains all the function prototypes
- * and the struncture definations used in the 0x1A. C - Hash
- * tables project.
- *
- * Author: Gizachew Bayness (Elec Crazy)
- *
- * Date Created: Jul 28 2022
- *
- */
+#ifndef _HASH_TABLE_H_
+#define _HASH_TABLE_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -24,14 +12,12 @@
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
- *
  */
-
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -41,20 +27,20 @@ typedef struct hash_node_s
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
- *
  */
-
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
@@ -72,11 +58,11 @@ void hash_table_delete(hash_table_t *ht);
 
 typedef struct shash_node_s
 {
-	char *key;
-	char *value;
-	struct shash_node_s *next;
-	struct shash_node_s *sprev;
-	struct shash_node_s *snext;
+char *key;
+char *value;
+struct shash_node_s *next;
+struct shash_node_s *sprev;
+struct shash_node_s *snext;
 } shash_node_t;
 
 /**
