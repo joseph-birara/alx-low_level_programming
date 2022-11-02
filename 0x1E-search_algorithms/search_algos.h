79ef3b01ac97514b4abd07582041d1a5906e05a5
joseph-birara <josephbirara92@gmail.com>
2022-11-03 00:59:44 +0300

    advanced tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,25 @@
-#ifndef SEARCH_ALGO_H
-#define SEARCH_ALGO_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-#include <stddef.h>
+/*
+ * File: search_algos.h
+ * Desc: This header file contains all the prototypes required for
+ * the 0x1E-search_algorithms project.
+ *
+ * Author: Gizachew Bayness (Elec Crazy)
+ * Date Created: Oct 31, 2022
+ *
+ */
+
+#include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *a, int i, int j);
+
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
